2684e8e1f15a86f20e516945fa86f0a94f8b219b
pavithraRanasinghe <paviranasinghe20@gmail.com>
2024-07-29 22:19:37 +0530

    Initial commit

---
+++ src/main/java/com/esoft/citytaxi/CityTaxiApplication.java
@@ -0,0 +1,13 @@
+package com.esoft.citytaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityTaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityTaxiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esoft/citytaxi/CityTaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esoft.citytaxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityTaxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
