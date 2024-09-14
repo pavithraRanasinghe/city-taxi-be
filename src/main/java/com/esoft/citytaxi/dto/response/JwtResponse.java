6104e8eda0524073da71335b35e909d1bcb7b77e
pavithraRanasinghe <paviranasinghe20@gmail.com>
2024-09-14 14:33:25 +0530

    Add user type

--- src/main/java/com/esoft/citytaxi/dto/response/JwtResponse.java
+++ src/main/java/com/esoft/citytaxi/dto/response/JwtResponse.java
@@ -1,5 +1,6 @@
 package com.esoft.citytaxi.dto.response;
 
+import com.esoft.citytaxi.enums.UserType;
 import com.esoft.citytaxi.models.AppUser;
 import lombok.Data;
 
@@ -13,6 +14,7 @@ public class JwtResponse implements Serializable {
     private final String lastName;
     private final String email;
     private final String token;
+    private final UserType userType;
 
     public JwtResponse(AppUser appUser, String token) {
         this.id = appUser.getId();
@@ -20,5 +22,6 @@ public class JwtResponse implements Serializable {
         this.lastName = appUser.getLastName();
         this.email = appUser.getUsername();
         this.token = token;
+        this.userType = appUser.getUserType();
     }
 }
